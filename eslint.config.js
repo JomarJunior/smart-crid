89d554af5cf886c119b63a52c893e3247b7a886b
Jomar Júnior de Souza Pereira <jomar.junior@cern.ch>
2025-07-01 20:21:55 -0300

    refactor: replace .eslintrc.json with eslint.config.js for improved configuration management

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+        ...globals.mocha,
+        // Hardhat globals
+        artifacts: "readonly",
+        contract: "readonly",
+        ethers: "readonly",
+        network: "readonly",
+        web3: "readonly",
+        hre: "readonly"
+      }
+    },
+    rules: {
+      "semi": ["error", "always"],
+      "quotes": ["warn", "double"],
+      "indent": ["warn", 2],
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "no-console": "warn",
+      "prefer-const": "error",
+      "no-var": "error"
+    }
+  },
+  {
+    ignores: [
+      "node_modules/",
+      "artifacts/",
+      "cache/",
+      "coverage/",
+      "typechain-types/",
+      "dist/",
+      "build/"
+    ]
+  }
+];
