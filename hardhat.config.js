254cdac0db26ef21f611e5414c8b71c92a79fb79
Jomar Júnior de Souza Pereira <jomar.junior@cern.ch>
2025-07-02 12:42:48 -0300

    add hardhat-ignition module and deploy script for CRIDAccessControl

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@openzeppelin/hardhat-upgrades");
+require("@nomicfoundation/hardhat-ignition");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
