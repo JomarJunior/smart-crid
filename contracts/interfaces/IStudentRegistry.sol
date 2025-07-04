c24975a94516b3cebec824d5ffe6d5d095ae7d3d
Jomar Júnior de Souza Pereira <jomar.junior@cern.ch>
2025-07-04 19:06:32 -0300

    refactor: Remove unused import from IStudentRegistry interface

--- contracts/interfaces/IStudentRegistry.sol
+++ contracts/interfaces/IStudentRegistry.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import {ICRIDAccessControl} from "./ICRIDAccessControl.sol";
-
 /**
  * @title IStudentRegistry
  * @dev Interface for student registry contract
